c72d102abbf5002b21a8f23b3914e6a9dd04c025
Manfred Riem <mriem@manorrock.com>
2023-09-03 07:14:00 -0500

    Fixes #60 - Add SmartFilterConfig (#61)

---
+++ src/main/java/cloud/piranha/smart/webapp/SmartFilterConfig.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2002-2023 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package cloud.piranha.smart.webapp;
+
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletContext;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Properties;
+
+/**
+ * A filter configuration that loads from a properties class.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class SmartFilterConfig implements FilterConfig {
+
+    /**
+     * Stores the properties.
+     */
+    protected Properties properties;
+    
+    /**
+     * Stores the Servlet context.
+     */
+    protected ServletContext servletContext;
+
+    public SmartFilterConfig(ServletContext servletContext, Properties properties) {
+        this.servletContext = servletContext;
+        this.properties = properties;
+    }
+
+    @Override
+    public String getFilterName() {
+        return properties.getProperty("filterName", "");
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return servletContext;
+    }
+
+    @Override
+    public String getInitParameter(String name) {
+        return properties.getProperty(name);
+    }
+
+    @Override
+    public Enumeration<String> getInitParameterNames() {
+        return (Enumeration<String>) properties.propertyNames();
+    }
+}
